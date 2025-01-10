e9d11cadeb4158745f3c55f841559426c3b3933b
shunwuyu <shunwu2001@163.com>
2025-01-10 16:57:20 +0800

    引入pinia,安装stylus,引入全局css

--- vue_react/v3-element/src/main.js
+++ vue_react/v3-element/src/main.js
@@ -6,8 +6,10 @@ import {
 } from 'element3'
 import 'element3/lib/theme-chalk/index.css'
 import router from './router'
+import { createPinia } from 'pinia'
 
 createApp(App)
   .use(router)
+  .use(createPinia())
   .use(ElButton) // UI 组件库 开发效率提升了
   .mount('#app')
